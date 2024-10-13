5a197937bfc0b3aab76101534ac74f45ccdb94a8
waweruedwin8 <waweruedwin8@gmail.com>
2024-10-14 00:48:45 +0300

    created the app.js and required the projects dependancies and modules ie routes, middleware,

---
+++ Taskit/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+dotenv.config();
+// import routes
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const userRoutes = require('./routes/userRoutes'); // Include user routes
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+app.use('/users', userRoutes); 
+//export app.js
+module.exports = app;
