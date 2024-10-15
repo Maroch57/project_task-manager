4999719d88b05d09ee184d6ae8fad83c47932598
waweruedwin8 <waweruedwin8@gmail.com>
2024-10-16 01:12:06 +0300

    added the server,js file to modularise the app.js

--- Taskit/app.js
+++ Taskit/app.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const passportConfig = require('./middleware/passportConfig'); // Import passport configuration
+const authRoutes = require('./routes/authRoutes');
+const jwtAuth = require('./middleware/jwtAuth');
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ const taskRoutes = require('./routes/taskRoutes');
 const userRoutes = require('./routes/userRoutes'); // Include user routes
 
 const app = express();
+
 
 // Middleware
 app.use(cors());
@@ -26,5 +29,11 @@ app.use(passport.session());
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
 app.use('/users', userRoutes); 
+
+// Protected route example
+app.get('/profile', jwtAuth, (req, res) => {
+       res.json({ message: `Welcome, ${req.user.email}` });
+   });
+   
 //export app.js
 module.exports = app;
