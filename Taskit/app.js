b2ad436ba76566040cb893a2ad4249379844dfaa
waweruedwin8 <waweruedwin8@gmail.com>
2024-10-21 06:46:18 +0300

    resolved merge issues

--- Taskit/app.js
+++ Taskit/app.js
@@ -9,7 +9,8 @@ dotenv.config();
 // import files
 const { User } = require('./src/models/User'); // Adjust the path to your User model accordingly
 const passportConfig = require('./src/middleware/passportConfig'); // Import passport configuration
-const jwtAuth = require('./src/middleware/jwtAuth');
+//const jwtAuth = require('./src/middleware/jwtAuth');
+const { jwtAuth, logUserInfo } = require('./src/middleware/jwtAuth');
 
 //catch google OAuth25Strategy errors
 console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
@@ -31,7 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(logUserInfo);
+app.use(jwtAuth);
 // Routes
 app.use('/auth', userRoutes);
 app.use('/tasks', taskRoutes);
