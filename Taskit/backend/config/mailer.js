a6a97ba6644db9a88bf6fd9dcf7ee3ff6b114585
waweruedwin8 <waweruedwin8@gmail.com>
2024-10-24 13:30:59 +0300

    mail handler

--- Taskit/backend/config/mailer.js
+++ Taskit/backend/config/mailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer');
 
-// Create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
+module.exports = transporter;
