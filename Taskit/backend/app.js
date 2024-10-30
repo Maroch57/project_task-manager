a8f5c400acc5335de4c6339debade3a52f789e27
waweruedwin8 <waweruedwin8@gmail.com>
2024-10-30 10:47:00 +0300

    ensured the endpoints are working

--- Taskit/backend/app.js
+++ Taskit/backend/app.js
@@ -1,12 +1,21 @@
+//app.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const routes = require('./routes');
 const cors = require('cors');
 require('dotenv').config();
+//addition
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use('/', routes);
+
+//addition
+app.get('/', (req, res) => {
+       res.sendFile(path.join(__dirname, '../frontend/index.html'));
+     });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
